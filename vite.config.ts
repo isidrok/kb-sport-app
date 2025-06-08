eddcbae5f0c710e6b454dfa84e554630bdf28d13
Isidrok <isistorre95@gmail.com>
2025-06-08 13:37:27 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [preact(), basicSsl()],
-  server: {
-    https: {},
-    host: "0.0.0.0", // This allows access from other devices on the network
-  },
+  base: "/kb-sport-app/",
+  plugins: [preact()],
 });
